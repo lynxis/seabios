487ef74fc6cb1b1020fb25c9bb3f166602ca926c
Alexander Couzens <lynxis@fe80.eu>
2015-03-06 03:18:01 +0100

    Drop Hostname + introduce git commit timestamp on clean git-tree builds

--- scripts/buildversion.sh
+++ scripts/buildversion.sh
@@ -2,16 +2,22 @@
 # Script to generate a C file with version information.
 OUTFILE="$1"
 VAR16MODE="$2"
-
+DATE=""
 # Extract version info
 if [ -d .git -o -f .git ]; then
     VERSION="`git describe --tags --long --dirty`"
+    if git update-index -q --refresh >/dev/null; \
+       git diff-index --quiet HEAD; then
+      DATE=$(git log --date=local --pretty=format:%ct -1)
+    else
+      DATE=$(date +%s)
+    fi
 elif [ -f .version ]; then
     VERSION="`cat .version`"
 else
     VERSION="?"
 fi
-VERSION="${VERSION}-`date +"%Y%m%d_%H%M%S"`-`hostname`"
+VERSION="${VERSION}-`date -d @$DATE +"%Y%m%d_%H%M%S"`-buildhost"
 echo "Version: ${VERSION}"
 
 # Build header file
